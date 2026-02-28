6780722f0f44cb4a1fe166b023ad1d177bd83eca
Endy14-sudo <u3778838036@gmail.com>
2026-02-28 06:15:50 +0100

    Update creatore-join.js

--- plugins/creatore-join.js
+++ plugins/creatore-join.js
@@ -1,24 +1,22 @@
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3}|inf)?/i;
-let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
-    if (!text) return m.reply(`令 Inserisci il link del gruppo.\n> *Esempio:* ${usedPrefix + command} <link> <numero di giorni | inf>.`);
-    let [_, code, expired] = text.match(linkRegex) || [];
-    if (!code) return m.reply('令 Link non valido.');
-    const isNumber = (x) => (x = parseInt(x), typeof x === 'number' && !isNaN(x));
-    let res = await conn.groupAcceptInvite(code);
-    if (expired === 'inf') {
-        m.reply(`令 Mi sono unito correttamente al gruppo senza una data di scadenza.`);
-    } else {
-        expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)));
-        m.reply(`令 Mi sono unito correttamente al gruppo per *${expired}* giorni.`);
-        let chats = global.db.data.chats[res];
-        if (!chats) chats = global.db.data.chats[res] = {};
-        if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24;
-    }
+
+let handler = async (m, { conn, text }) => {
+  let linkRegex = /chat\.whatsapp\.com\/([0-9A-Za-z]{20,24})/i;
+  let [, code] = text.match(linkRegex) || [];
+  if (!code) throw 'Link non valido!';
+
+  // Messaggio unico stilizzato
+  await m.reply('♊ 𝐙𝚬𝑌𝐍𝟎 𝐁𝐎𝐓 ♊\n\n🚪 sta entrando nel gruppo...');
+
+  try {
+    await conn.groupAcceptInvite(code);
+  } catch (e) {
+    throw 'Errore: il bot è già nel gruppo o il link non è valido.';
+  }
 };
 
-handler.help = ['join *<link> <giorni | inf>*'];
-handler.tags = ['creatore'];
+handler.help = ['join <chat.whatsapp.com>'];
+handler.tags = ['owner'];
 handler.command = ['join'];
-handler.owner = true;
+handler.rowner = true;
 
 export default handler;
